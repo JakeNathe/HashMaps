88f942ca013f5835034762cbc41c45c74892c445
JakeNathe <Jakenathe1@gmail.com>
2023-05-31 22:53:05 -0500

    updated capacity in resize method

--- hash_map_sc.py
+++ hash_map_sc.py
@@ -148,8 +148,8 @@ class HashMap:
         if 1 > new_capacity:
             return
         # capacity must be a prime number
-        if self.table_load() > 1:
-            new_capacity = self._size
+        if new_capacity < self._size:
+            new_capacity = self._size + 1
         if self._is_prime(new_capacity) is False:
             new_capacity = self._next_prime(new_capacity)
 
