70a3b4b444c67cd67c98108d4accc86e28da34e5
JakeNathe <Jakenathe1@gmail.com>
2023-06-01 23:28:33 -0500

    resize function

--- hash_map_oa.py
+++ hash_map_oa.py
@@ -150,27 +150,33 @@ class HashMap:
         if self._size > new_capacity:
             return
 
+        # capacity must be a prime number
+        if self._is_prime(new_capacity) is False:
+            new_capacity = self._next_prime(new_capacity)
+
         old_buckets = self._buckets
-        old_size = self._size
+        old_capacity = self._capacity
         self._capacity = new_capacity
         self._buckets = DynamicArray()
+
         for i in range(self._capacity):
             self._buckets.append(None)
         self._size = 0
-        for i in range(old_size):
-            kv_pair = old_buckets[i]
-            if kv_pair is not None:
-                self.put(kv_pair.key, kv_pair.value)
 
-        #
+        for i in range(old_capacity):
+            index = old_buckets[i]
+            if index is not None and index.is_tombstone is False:
+                self.put(index.key, index.value)
+
+
         # # new hash map with new capacity
         # updated_map = HashMap(new_capacity, self._hash_function)
         #
         # # capacity must be a prime number
         # if self._is_prime(new_capacity) is False:
         #     new_capacity = self._next_prime(new_capacity)
-        #
-        # # iterate over the buckets of temp map and insert into the actual map
+
+        # iterate over the buckets of temp map and insert into the actual map
         # for index in range(self._capacity):
         #     hash_obj = self._buckets[index]
         #     if hash_obj is not None and hash_obj.is_tombstone is False:
